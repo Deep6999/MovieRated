d1b722aaa5cbcf38c2855815d6dc5adcee1efdb0
Deep6999 <71369810+Deep6999@users.noreply.github.com>
2021-10-29 17:14:37 +0530

    Update Servicey.java

--- app/src/main/java/com/devwonk/movierated/request/Servicey.java
+++ app/src/main/java/com/devwonk/movierated/request/Servicey.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Servicey {
-private static     Retrofit.Builder retrofitB = new Retrofit.Builder()
+private static Retrofit.Builder retrofitB = new Retrofit.Builder()
             .baseUrl(Credentials.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create());
 private static Retrofit retrofit = retrofitB.build();
